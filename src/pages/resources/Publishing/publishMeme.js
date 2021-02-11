0c8c33a566038e0b9103bc774e4379f96e921c39
m1st3rnutso <georgemichaelcallan@gmail.com>
2021-02-11 13:18:56 +1000

    Delete src/pages/resources/Publishing directory
    Mistaken directory

--- src/pages/resources/Publishing/publishMeme.js
+++
@@ -1,9 +0,0 @@
-function publishMeme(topText, bottomText, randomMemeImage) {
-    return (<div className="meme">
-        <img src={randomMemeImage.url}/>
-        <h1 className="topText">{topText}</h1>
-        <h2 className="bottomText">{bottomText}</h2>
-    </div>)
-}
-
-export default publishMeme
